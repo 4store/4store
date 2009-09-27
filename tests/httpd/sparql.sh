70aa7d1d8b7730f6484787d8ff202d246b03f7d7
Steve Harris <steve.harris@garlik.com>
2009-09-27 15:48:57 +0100

    Added tests for 4s-httpd

---
+++ tests/httpd/sparql.sh
@@ -0,0 +1,31 @@
+function uriescape {
+	escaped=`echo "$1" | sed 's/ /%20/g; s/\*/\\*/g; s/{/\\\{/g; s/}/\\\}/g; s/\?/%3f/g; s/&/%38/g; s/+/%2b/g; s/"/\\"/g'`
+}
+
+# usage: sparql $endpoint $query
+function sparql {
+	uriescape "$2";
+	curl -s -H "Accept: text/plain" "$1/sparql/?query=$escaped"
+}
+
+# usage: put $endpoint $file $mime-type $model
+function put {
+	uriescape $4;
+	curl -s -T "$2" -H "Content-Type: $3" "$1/data/$escaped" | sed 's/ v[.0-9a-z-]*/ [VERSION]/'
+}
+
+# usage: post $endpoint $data $mime-type $model
+function post {
+	uriescape "$3";
+	emime=$escaped
+	uriescape "$4";
+	egraph=$escaped;
+	uriescape "$2"
+	curl -s -H 'Accept: text/plain' -d "mime-type=$emime" -d "graph=$egraph" -d "data=$escaped" "$1/data/" | sed 's/ v[.0-9a-z-]*/ [VERSION]/'
+}
+
+# usage: delete $endpoint $model
+function delete {
+	uriescape $2;
+	curl -s -X 'DELETE' $1/data/$escaped | sed 's/ v[.0-9a-z-]*/ [VERSION]/'
+}
