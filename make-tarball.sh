3ebc3af661d00c7d3d5c874b75c6c445b4b67353
Steve Harris <steve.harris@garlik.com>
2010-11-01 17:19:55 +0000

    Removing some generated files

--- make-tarball.sh
+++ make-tarball.sh
@@ -7,7 +7,8 @@ name="4store-${revision}"
 rm -f tests/{query,import}/results/*
 rm -rf /tmp/${name}
 cp -r . /tmp/${name}
-echo "export gitrev = ${revision}" > /tmp/${name}/src/rev.mk
+echo "echo -n ${revision}" > /tmp/${name}/version.sh
+chmod 755 /tmp/${name}/version.sh
 (cd /tmp && tar cvfz ${name}.tar.gz -h --exclude .git --exclude .gitignore --exclude dawg --exclude '*.tar.gz' --exclude '*.dmg' --exclude '*.app' --exclude '*.o' ${name})
 mv /tmp/${name}.tar.gz .
 rm -rf /tmp/${name}
