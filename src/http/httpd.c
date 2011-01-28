f7f03cf087184e70f3121425c334d877aca80ff9
Steve Harris <steve.harris@garlik.com>
2011-01-28 17:20:44 +0000

    Make version error message clearer

--- src/http/httpd.c
+++ src/http/httpd.c
@@ -433,7 +433,7 @@ static void http_import_start(client_ctxt *ctxt)
     http_send(ctxt, "HTTP/1.0 500 Not Implemented\r\n");
     http_send(ctxt, "Server: 4s-httpd/" GIT_REV "\r\n");
     http_send(ctxt, "Content-Type: text/plain; charset=utf-8\r\n\r\n");
-    http_send(ctxt, "SPARQL Update support requires rasqal 0.9.17+\n");
+    http_send(ctxt, "SPARQL Update support requires rasqal 0.9.18 or newer\n");
     http_send(ctxt, "\n");
     http_close(ctxt);
 #endif
